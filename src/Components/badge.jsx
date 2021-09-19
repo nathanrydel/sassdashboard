6d6ebd71f2049417b3a2da1c5e3a23d659d4865e
nathanrydel <nathanrydel@gmail.com>
2021-09-19 16:59:30 -0700

    Create badge element

---
+++ src/Components/badge.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function badge() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+const Div = styled.div``;
+
+
+export default badge
