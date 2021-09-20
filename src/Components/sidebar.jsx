c0f3cb707742fba03cc222f4a7d505f59f0a452a
Nathan Rydel <nathanrydel@gmail.com>
2021-09-19 22:41:05 -0700

    Initial Sidebar functionality

--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Badge from './Badge';
+import ProfileImage from  "..//assets/ProfileImage01.jpg";
+import { darkThemeColor } from '../utils';
+
 
 function Sidebar() {
-    return (
-        <div>
-            
-        </div>
-    )
+    return (<Container>
+            <ProfileContainer>
+                <Avatar src= {ProfileImage} />
+                <Name>Ryan Anderson</Name>
+                <Badge content="Pro" />
+            </ProfileContainer>
+    </Container>
+    );
 }
+
+const Container = styled.div`
+    width:20%;
+    height: 100% !important;
+    border-radius: 1rem;
+    background-color: #030e1f;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+`;
+
+const ProfileContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+const Avatar = styled.img`
+    height: 6rem;
+    border-radius: 5rem;
+    margin-top: 18%auto;
+`;
+
+const Name = styled.h1`
+    color: white;
+    font: 1.25rem;
+    font-weight: 400;
+    margin: .75rem 0 .5rem 0;
+`;
+
+const Container = styled.div``;
 
 export default Sidebar
