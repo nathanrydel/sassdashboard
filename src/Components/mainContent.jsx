9718784cc4faccf963044ed0b5afe84f6cb19dd7
nathanrydel <nathanrydel@gmail.com>
2021-09-20 15:14:12 -0700

    Create MainContent structure

--- src/Components/mainContent.jsx
+++ src/Components/mainContent.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from './Navbar';
 
 function MainContent() {
     return (
-        <div>
-            
-        </div>
-    )
+        <Container>
+            <Navbar />
+            <SubContainer>
+                <SectionOne>
+                    <ColumnOne1></ColumnOne1>
+                    <ColumnTwo1></ColumnTwo1>
+                </SectionOne>
+                <SectionTwo>
+                    <ColumnOne2></ColumnOne2>
+                    <ColumnTwo2></ColumnTwo2>
+                </SectionTwo>
+            </SubContainer>
+        </Container>
+    );
 }
+
+const Container = styled.div``;
+
+const SubContainer = styled.div``;
+
+const SectionOne = styled.div``;
+
+const SectionTwo = styled.div``;
+
+const ColumnOne1 = styled.div``;
+
+const ColumnOne2 = styled.div``;
+
+const ColumnTwo1 = styled.div``;
+
+const ColumnTwo2 = styled.div``;
 
 export default MainContent
