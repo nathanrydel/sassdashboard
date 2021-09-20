3e68e59807c6eb6d1ee1bcab5cf01e96055bc03f
Nathan Rydel <nathanrydel@gmail.com>
2021-09-20 16:27:49 -0700

    Style SubContainer, TitleText, SectionOne Style SectionTwo, ColumnOne1, ColumnTwo1

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -28,17 +28,48 @@ const Container = styled.div`
     margin: 1rem 8rem 1rem 4rem;
 `;
 
-const SubContainer = styled.div``;
+const SubContainer = styled.div`
+    margin: .5rem 0;
+    height: 80%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+`;
 
-const SectionOne = styled.div``;
+const TitleText = styled.h3`
+    height: 20%;
+`;
 
-const SectionTwo = styled.div``;
 
-const ColumnOne1 = styled.div``;
+const SectionOne = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 35%;
+    width: 100%;
+    gap: 2rem;
+
+`;
+
+const SectionTwo = styled.div`
+    display: flex;
+    gap: 2rem;
+    height: 26vh;
+`;
+
+const ColumnOne1 = styled.div`
+    display: flex;
+    gap: 2rem;
+`;
 
 const ColumnOne2 = styled.div``;
 
-const ColumnTwo1 = styled.div``;
+const ColumnTwo1 = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 105%;
+    width: 100%;
+`;
 
 const ColumnTwo2 = styled.div``;
 
