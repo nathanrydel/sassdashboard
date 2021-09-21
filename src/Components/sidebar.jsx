e827fd5d6f9a7a795915c960459767e2af8a796f
Nathan Rydel <nathanrydel@gmail.com>
2021-09-20 17:13:01 -0700

    Prettier formatting of Sidebar Fix Links integration

--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,132 +1,133 @@
 import React from 'react';
 import styled from 'styled-components';
-import { RiHomeLine, RiFileCopyLine } from "react-icons/ri";
-import { FaWallet } from "react-icons/fa";
-import { AiOutlinePieChart } from "react-icons/ai";
+import { RiHomeLine, RiFileCopyLine } from 'react-icons/ri';
+import { FaWallet } from 'react-icons/fa';
+import { AiOutlinePieChart } from 'react-icons/ai';
 import Badge from './Badge';
-import ProfileImage from  "..//assets/ProfileImage01.jpg";
+import ProfileImage from '..//assets/ProfileImage01.jpg';
 import { darkThemeColor } from '../utils';
 
-
 function Sidebar() {
-    return (
+  return (
     <Container>
-            <ProfileContainer>
-                <Avatar src= {ProfileImage} />
-                <Name>Ryan Anderson</Name>
-                <Badge content="Pro" />
-            </ProfileContainer>
-            <LinksContainer>
-                <Links>
-                    <Link>
-                        <RiHomeLine />
-                        <h3>Dashboard</h3>
-                    </Link>
-                    <Link>
-                        <RiFileCopyLine />
-                        <h3>Projects</h3>
-                    </Link>
-                    <Link>
-                        <FaWallet />
-                        <h3>Invoices</h3>
-                    </Link>
-                    <Link>
-                        <AiOutlinePieChart />
-                        <h3>Reports</h3>
-                    </Link>
-                </Links>
-            </LinksContainer>
-            <ContactContainer>
-                <span> Have a problem?</span>
-                <a href="/" target="_blank">Contact us</a>
-            </ContactContainer>
+      <ProfileContainer>
+        <Avatar src={ProfileImage} />
+        <Name>Ryan Anderson</Name>
+        <Badge content='Pro' />
+      </ProfileContainer>
+      <LinksContainer>
+        <Links>
+          <Link>
+            <RiHomeLine />
+            <h3>Dashboard</h3>
+          </Link>
+          <Link>
+            <RiFileCopyLine />
+            <h3>Projects</h3>
+          </Link>
+          <Link>
+            <FaWallet />
+            <h3>Invoices</h3>
+          </Link>
+          <Link>
+            <AiOutlinePieChart />
+            <h3>Reports</h3>
+          </Link>
+        </Links>
+      </LinksContainer>
+      <ContactContainer>
+        <span> Have a problem?</span>
+        <a href='/' target='_blank'>
+          Contact us
+        </a>
+      </ContactContainer>
     </Container>
-    );
+  );
 }
 
 const Container = styled.div`
-    width:20%;
-    height: 100% !important;
-    border-radius: 1rem;
-    background-color: #030e1f;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 2rem;
-    min-width: 180px;
-    max-width: 240px;
+  width: 20%;
+  height: 100% !important;
+  border-radius: 1rem;
+  background-color: #030e1f;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  min-width: 180px;
+  max-width: 240px;
 `;
 
 const ProfileContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 const Avatar = styled.img`
-    height: 7rem;
-    width: 60%;
-    border-radius: 50%;
-    margin-top: 18%;
-    cursor: pointer;
+  height: 7rem;
+  width: 60%;
+  border-radius: 50%;
+  margin-top: 18%;
+  cursor: pointer;
 `;
 
 const Name = styled.h1`
-    color: white;
-    font-size: 1.5rem;
-    font-weight: 400;
-    margin: .75rem 0 .5rem 0;
+  color: white;
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin: 0.75rem 0 0.5rem 0;
 `;
 
 const LinksContainer = styled.div`
-    background-color: ${darkThemeColor};
-    height: 100%;
-    width: 100%;
-    border-radius: 1rem;
+  background-color: ${darkThemeColor};
+  height: 100%;
+  width: 100%;
+  border-radius: 1rem;
 `;
 
 const Links = styled.ul`
-    list-style-type: none;
-    display: flex;
-    flex-direction: column;
-    padding-top: 1.5rem;
-    height: 48%;
-    margin-right: 15px;
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  padding-top: 1.5rem;
+  height: 48%;
+  margin-right: 15px;
 `;
 
 const Link = styled.li`
-    margin-left: 25%;
-    margin-bottom: 2rem;
-    display: flex;
-    gap: 1rem;
-    color: #e4e4e4;
-    cursor: pointer;
-    h3 {
-        font-weight: 300;
-    }
-    svg {
-        font-size: 1.2rem;
-        margin-top: 2%;
-    }
+  margin-left: 25%;
+  margin-bottom: 2rem;
+  display: flex;
+  gap: 1rem;
+  color: #e4e4e4;
+  cursor: pointer;
+  h3 {
+    font-weight: 300;
+  }
+  svg {
+    font-size: 1.2rem;
+    margin-top: 2%;
+  }
 `;
 
 const ContactContainer = styled.div`
-    width: 60%;
-    background-color: #030e1f;
-    color: #e4e4e4;
-    height: 20%;
-    margin: auto;
-    border-radius: 1rem;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    margin-bottom: 2rem;
-    a {
-        color: white;
-        text-decoration: none;
-    }
+  width: 60%;
+  background-color: #030e1f;
+  color: #e4e4e4;
+  height: 20%;
+  margin: auto;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  margin-bottom: 2rem;
+  a {
+    color: white;
+    text-decoration: none;
+  }
 `;
 // const Container = styled.div``;
 
-export default Sidebar
+export default Sidebar;
