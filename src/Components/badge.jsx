cef07079951bbad9148625b3d1ad93e855ec5cae
Nathan Rydel <nathanrydel@gmail.com>
2021-09-20 17:11:16 -0700

    Prettier formatting of Badge

--- src/Components/badge.jsx
+++ src/Components/badge.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
-import { themeColor } from "../utils"
+import { themeColor } from '../utils';
 
 function Badge({ content }) {
-    return (
-        <Div> { content }   
-        </Div>
-    )
+  return <Div> {content}</Div>;
 }
 
 const Div = styled.div`
-    padding: 0.3rem 1rem;
-    border-radius: 1rem;
-    font-weight: 500;
-    color: white;
-    background-color: ${themeColor};
-    cursor: pointer;
+  padding: 0.3rem 1rem;
+  border-radius: 1rem;
+  font-weight: 500;
+  color: white;
+  background-color: ${themeColor};
+  cursor: pointer;
 `;
 
-
-export default Badge
+export default Badge;
