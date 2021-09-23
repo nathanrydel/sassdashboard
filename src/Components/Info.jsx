f60b5f4fd560daf08eee7a7852a7856018f71110
nathanrydel <nathanrydel@gmail.com>
2021-09-22 20:07:41 -0700

    Style Info component

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -5,9 +5,9 @@ import { cardShadow, hoverEffect, themeColor } from '../utils';
 
 function info() {
   return (
-    <infoCard>
+    <InfoCard>
       <Card>
-        <CardConent>
+        <CardContent>
           <Row>
             <Digit> 98</Digit>
             <InfoContainer>
@@ -15,7 +15,7 @@ function info() {
               <SubTitle>In top 15%</SubTitle>
             </InfoContainer>
           </Row>
-        </CardConent>
+        </CardContent>
       </Card>
       <Card>
         <CardContent>
@@ -32,8 +32,66 @@ function info() {
           </Row>
         </CardContent>
       </Card>
-    </infoCard>
+    </InfoCard>
   );
 }
+
+const InfoCard = styled.div`
+  height: 100%;
+  width: 12rem;
+  background-color: white;
+  border-radius: 1rem;
+  padding: 1rem;
+  color: white;
+  box-shadow: ${cardShadow};
+  transition: 0.4s ease-in-out;
+  &:hover {
+    box-shadow: ${hoverEffect};
+  }
+`;
+
+const Card = styled.div`
+  background-color: rgba(183, 194, 243, 0.3);
+  border-radius: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const CardContent = styled.div`
+  padding: 0.5rem 1rem 0.3rem 1rem;
+`;
+
+const InfoContainer = styled.div`
+  margin-left: 1rem;
+`;
+
+const Digit = styled.div`
+  background-color: ${themeColor};
+  padding: 0.8rem 1rem;
+  font-size: 1.3rem;
+  border-radius: 1rem;
+`;
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  ${({ justify }) =>
+    justify &&
+    `
+    justify-content: space-around;
+    width: 90%
+  `}
+`;
+
+const Title = styled.h3`
+  color: black;
+`;
+
+const SubTitle = styled.h5`
+  color: #333;
+  font-weight: normal;
+`;
+
+// const InfoCard = styled.div``;
 
 export default info;
