681d969a32f665d3c156108642b5e8e6d2898014
nathanrydel <nathanrydel@gmail.com>
2021-09-22 20:24:33 -0700

    Update and Style Badge component

--- src/Components/Badge.jsx
+++ src/Components/Badge.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { themeColor } from '../utils';
 
-function Badge({ content }) {
-  return <Div> {content}</Div>;
+function Badge({ content, clean = false, glow = false, paid = false, late = false }) {
+  return (
+    <Div glow={glow} paid={paid} late={late}>
+      {' '}
+      {content}
+    </Div>
+  );
 }
 
 const Div = styled.div`
@@ -13,6 +18,33 @@ const Div = styled.div`
   color: white;
   background-color: ${themeColor};
   cursor: pointer;
+
+  ${({ clean }) =>
+    clean &&
+    `background-color: transparent;
+    border: 0.05rem solid ${themeColor};
+     color:${themeColor};`}
+  ${({ glow }) =>
+    glow &&
+    `
+        font-size:0.8rem;
+        padding:0.2rem 0.5rem;
+        font-weight:normal;
+          background-color: rgba(109, 134, 245, 0.192);
+        color:#2f233d;
+    `}
+    ${({ paid }) =>
+    paid &&
+    `
+        background-color:#70e00041;
+        color:#70e000;
+    `}
+${({ late }) =>
+    late &&
+    `
+        background-color:#ff595e41;
+        color:#ff595e;
+    `}
 `;
 
 export default Badge;
