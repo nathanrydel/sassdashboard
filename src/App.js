1b082a562a2ca30597ca018f61192adf5f77fa35
Nathan Rydel <nathanrydel@gmail.com>
2021-09-24 20:47:17 -0700

    Fix React import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import styled from 'styled-components';
 import Sidebar from './Components/Sidebar';
 import MainContent from './Components/MainContent';
