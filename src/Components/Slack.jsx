5469387ccde3956a425fb35afa1fe466856e3e75
Nathan Rydel <nathanrydel@gmail.com>
2021-09-24 20:37:07 -0700

    Style CardContent

--- src/Components/Slack.jsx
+++ src/Components/Slack.jsx
@@ -35,7 +35,13 @@ const JoinChannel = styled.div`
   }
 `;
 
-const CardContent = styled.div``;
+const CardContent = styled.div`
+  margin: 1rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const SlackCard = styled.div``;
 
