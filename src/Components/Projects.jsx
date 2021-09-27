410ab7611036e7d7c226d4731a5d0090b3c846fb
Nathan Rydel <nathanrydel@gmail.com>
2021-09-26 22:47:04 -0700

    Create structure for component

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,7 +5,29 @@ import ProfileImage02 from '..//assets/ProfileImage03.jpg';
 import { cardShadow, hoverEffect, themeColor } from '..//utils';
 
 function Projects() {
-  return <div></div>;
+  return (
+    <UserProjects>
+      <Project>
+        <Profile>
+          <img src={ProfileImage} alt='user profile' />
+        </Profile>
+        <Description>
+          <Title>Branding Photography Studio</Title>
+          <SubTitle>1 day remaining</SubTitle>
+        </Description>
+      </Project>
+      <Project>
+        <Profile>
+          <img src={ProfileImage02} alt='user profile' />
+        </Profile>
+        <Description>
+          <Title>eCommerce Logo redesign and branding</Title>
+          <SubTitle>4 days remaining</SubTitle>
+        </Description>
+      </Project>
+      <AllProjects>See all projects</AllProjects>
+    </UserProjects>
+  );
 }
 
 export default Projects;
