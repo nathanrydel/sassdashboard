2a100c406e021291660e8e53c345ca6565999d17
Nathan Rydel <nathanrydel@gmail.com>
2021-09-26 22:43:13 -0700

    Import Planning of all features

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import ProfileImage from '..//assets/ProfileImage02.jpg';
+import ProfileImage02 from '..//assets/ProfileImage03.jpg';
+import { cardShadow, hoverEffect, themeColor } from '..//utils';
+
+function Projects() {
+  return <div></div>;
+}
+
+export default Projects;
